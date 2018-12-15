07fc4ba56d92eb8b384dc8d82a28abcc1a65b56d
VictorArd <214tw@protonmail.com>
2018-12-15 13:42:44 +0100

    Ajout de l'exercice 11

--- exo_10.rb
+++ exo_10.rb
@@ -1,3 +1,3 @@
 puts "Quel âge à tu ?"
 age = gets.chomp.to_i
-puts "#{age - 1}"
+puts "Votre âge en 2017 était de #{age - 1} ans"
---
+++ exo_11.rb
@@ -0,0 +1,3 @@
+puts "Donnez moi un nombre"
+nombre = gets.chomp.to_i
+puts "Salut, ça farte ?" * nombre
