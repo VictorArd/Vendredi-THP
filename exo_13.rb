4a84c5795b18c2bea9ab2c9c47b3efb3bafe67dd
VictorArd <214tw@protonmail.com>
2018-12-15 14:31:30 +0100

    Ajout exo_13.rb

---
+++ exo_13.rb
@@ -0,0 +1,18 @@
+puts "Qu'elle est ton année de naissance?"
+
+naissance = gets.chomp.to_i
+
+puts naissance
+
+
+
+annee = 2018
+i= naissance
+
+
+annee.times do |naissance|
+	puts i
+	i = i + 1
+	break if i >= 2018
+end
+
