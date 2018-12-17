e71c70a304137ff05731c5074a14e0a6217ca11c
VictorArd <214tw@protonmail.com>
2018-12-17 12:13:48 +0100

    Ajout de l'exercice 20

---
+++ exo_20.rb
@@ -0,0 +1,13 @@
+puts "Choisi un nombre entre 1 et 25"
+print ">"
+nombre = gets.chomp.to_i
+
+puts "Voici la pyramide :"
+
+i = "#"
+
+nombre.times  do |pyramide|
+		puts i
+		i = "#" + i
+		break if pyramide > 25
+end
